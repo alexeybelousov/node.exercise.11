dcbd93b9a36b8fc5c1b4ac8a9f5cca43638ee130
Alexey Belousov <belousov.fg@yandex.ru>
2018-04-16 12:17:23 +0300

    Add rooms

--- index.js
+++ index.js
@@ -1,24 +1,43 @@
-var app = require('express')();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+const app = require('express')();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
 
-app.get('/', function(req, res){
-  res.sendFile(__dirname + '/index.html');
+app.set('view engine', 'ejs');
+
+app.get('/', function(req, res) {
+  const rooms = [
+      { title: 'Room 1', name: 'room1' },
+      { title: 'Room 2', name: 'room2' },
+      { title: 'Room 3', name: 'room3' }
+    ];
+
+  res.render('./index', { rooms });
+});
+
+app.get('/rooms/*', (req, res) => {
+  res.render('./chat');
 });
 
 io.on('connection', function(socket) {
   console.log('a user connected');
 
-  // send info about new user connected
-  //io.emit('new user');
-  socket.broadcast.emit('user-joined');
-
-  socket.on('chat message', function(msg){
-    io.emit('chat message', msg);
+  let room;
+  app.get('/rooms/:room', (req, res) => {
+    room = req.params.room;
   });
 
-  socket.on('disconnect', function(){
+  socket.join(room);
+
+  socket.broadcast.to(room).emit('user-joined');
+
+  socket.on('chat message', function(msg) {
+    io.to(room).emit('chat message', msg)
+  });
+
+  socket.on('disconnect', function() {
     console.log('user disconnected');
+
+    socket.leave(room);
   });
 });
 
